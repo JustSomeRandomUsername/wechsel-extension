df020a72b8d9ef74a315fc912022fcb259f00272
Uhrendoktor <36703334+Uhrendoktor@users.noreply.github.com>
2024-03-31 17:16:31 +0200

    indicator foldout proposal

--- extension.js
+++ extension.js
@@ -21,6 +21,7 @@
 // const GETTEXT_DOMAIN = 'project-switcher-extension';
 
 import St from 'gi://St';
+import Atk from 'gi://Atk';
 import GObject from 'gi://GObject';
 import Gio from 'gi://Gio';
 import GLib from 'gi://GLib';
@@ -28,99 +29,187 @@ import Meta from 'gi://Meta';
 import Shell from 'gi://Shell';
 import Clutter from 'gi://Clutter';
 
-import * as BoxPointer from 'resource:///org/gnome/shell/ui/boxpointer.js';
-import { wm, panel, layoutManager} from 'resource:///org/gnome/shell/ui/main.js';
+import { wm, panel } from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
 import { getConfig } from './util/utils.js';
 import { ProjectSwitcherPopup } from './util/projectSwitcher.js'
 
-// const Me = imports.misc.extensionUtils.getCurrentExtension();
-// const Dialog = imports.ui.dialog;
-// const ModalDialog = imports.ui.modalDialog;
-// const ExtensionUtils = imports.misc.extensionUtils;
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
-
-// const BoxPointer = imports.ui.boxpointer;
-// const Main = imports.ui.main;
-// const PanelMenu = imports.ui.panelMenu;
-// const PopupMenu = imports.ui.popupMenu;
-// const Util = imports.misc.util;
-// const Clutter = imports.gi.Clutter;
-
-// const { wm } = imports.ui.main;
-
-// const { getConfig } = Me.imports.util.utils;
-// const { ProjectSwitcherPopup } = Me.imports.util.projectSwitcher;
-
-
-// const _ = ExtensionUtils.gettext;
 
 const interfaceXml = `
     <node>
         <interface name="org.gnome.shell.extensions.prjchange.service">
             <method name="Reload"/>
         </interface>
-    </node>`;
+    </node>
+`;
 
-// class PopupMenuSection extends PopupMenu.PopupMenuBase {
-//     constructor(sourceActor, arrowAlignment, arrowSide) {
-//         super(sourceActor, arrowAlignment, arrowSide);
-//         this._arrowAlignment = arrowAlignment;
-//         this._arrowSide = arrowSide;
 
-//         this.actor = this.box;
-//         this.actor._delegate = this;
-//         this.isOpen = true;
-//         this._boxPointer = new BoxPointer.BoxPointer(arrowSide);
-//         this.actor = this._boxPointer;
-//         this.actor._delegate = this;
+const FoldoutChildren = GObject.registerClass(
+    class FoldoutChildren extends St.BoxLayout {
+    _init() {
+        super._init({
+            vertical: true,
+            x_expand: true,
+            y_expand: true,
+        });
+        this.set_style('padding-left: 5px;');
+    }
+});
 
-//         this._boxPointer.bin.set_child(this.box);
+const Foldout = GObject.registerClass({
+    Properties: {
+        'unfolded': GObject.ParamSpec.boolean(
+            'unfolded', 'unfolded', 'unfolded',
+            GObject.ParamFlags.READWRITE,
+            false
+        ),
+    }},
+    class Foldout extends PopupMenu.PopupBaseMenuItem {
+    _init(
+        name,
+    ) {
+        super._init({
+            activate: true,
+            hover: true,
+            can_focus: true,
+            style_class: 'popup-menu-item',
+        });
 
-//         this.actor.add_style_class_name('popup-menu-section');
-//     }
+        this.box = new St.BoxLayout({
+            vertical: true,
+            x_expand: true,
+            y_expand: true
+        });
 
-//     open(animate) {
-//         if (this.isOpen)
-//             return;
+        this.header_box = new St.BoxLayout({
+            vertical: false,
+            x_expand: true,
+            y_expand: true,
+        });
 
-//         if (this.isEmpty())
-//             return;
+        this._icon = new St.Icon({
+            icon_name: 'pan-end-symbolic',
+            icon_size: 16,
+            style_class: 'popup-menu-icon',
+        });
+        this._label = St.Button.new_with_label(_(name));
+        this._ornament = new St.Icon({
+            icon_name: 'ornament-check-symbolic',
+            icon_size: 16,
+            style_class: 'popup-menu-icon',
+        });
 
-//         if (!this._systemModalOpenedId) {
-//             this._systemModalOpenedId =
-//                 layoutManager.connect('system-modal-opened', () => this.close());
-//         }
 
-//         this.isOpen = true;
+        this.header_box.add_child(this._icon);
+        this.header_box.add_child(this._label);
+        this.header_box.add_child(this._ornament);
 
-//         this._boxPointer.setPosition(this.sourceActor, this._arrowAlignment);
-//         this._boxPointer.open(animate);
+        this.hideOrnament();
 
-//         this.actor.get_parent().set_child_above_sibling(this.actor, null);
+        this.child_container = new FoldoutChildren(this);
 
-//         this.emit('open-state-changed', true);
-//     }
+        this.box.add_child(this.header_box);
+        this.box.add_actor(this.child_container);
 
-//     close(animate) {
-//         if (this._activeMenuItem)
-//             this._activeMenuItem.active = false;
+        this.close();
 
-//         if (this._boxPointer.visible) {
-//             this._boxPointer.close(animate, () => {
-//                 this.emit('menu-closed');
-//             });
-//         }
+        this.add_accessible_state(Atk.StateType.EXPANDABLE);
+        this.add_actor(this.box);
+    }
 
-//         if (!this.isOpen)
-//             return;
+    activate(event) {
+        if (this.unfolded) {
+            this.close();
+        } else {
+            this.open();
+        }
+    }
 
-//         this.isOpen = false;
-//         this.emit('open-state-changed', false);
-//     }
-// };
+    addMenuItem(item) {
+        this.child_container.add_actor(item);
+
+        // disable hover when the child container is hovered and this item is hovered
+        if (item.connect) {
+            // disable hover when the child container is hovered and this item is hovered
+            item.connect('enter-event', () => {
+                this.set_hover(false);
+            });
+
+            // close menu when left arrow key is pressed on child
+            item.connect('key-press-event', (actor, event) => {
+                let symbol = event.get_key_symbol();
+                if (symbol === Clutter.KEY_Left) {
+                    this.close();
+                    // focus this
+                    this.grab_key_focus();
+                    return Clutter.EVENT_STOP;
+                }
+                // handle up down focus
+                if (symbol === Clutter.KEY_Up) {
+                    let prev = item.get_previous_sibling();
+                    if (prev) {
+                        prev.grab_key_focus();
+                    } else {
+                        this.grab_key_focus();
+                    }
+                    return Clutter.EVENT_STOP;
+                }
+                if (symbol === Clutter.KEY_Down) {
+                    let next = item.get_next_sibling();
+                    if (next) {
+                        next.grab_key_focus();
+                    } else {
+                        this.get_next_sibling().grab_key_focus();
+                    }
+                    return Clutter.EVENT_STOP;
+                }
+            });
+        }
+    }
+
+    open() {
+        this.unfolded = true;
+        this._icon.icon_name = 'pan-down-symbolic';
+        this.child_container.show();
+        this.notify("unfolded");
+    }
+
+    close() {
+        this.unfolded = false;
+        this._icon.icon_name = 'pan-end-symbolic';
+        this.child_container.hide();
+        this.notify("unfolded");
+    }
+
+    vfunc_key_press_event(event) {
+        let symbol = event.get_key_symbol();
+
+        if (symbol === Clutter.KEY_Right) {
+            this.open();
+            // focus the first child in the child_container
+            this.child_container.get_first_child().grab_key_focus();
+            return Clutter.EVENT_STOP;
+        } else if (symbol === Clutter.KEY_Left && !this.unfolded) {
+            this.close();
+            // focus this
+            this.grab_key_focus();
+            return Clutter.EVENT_STOP;
+        }
+
+        return super.vfunc_key_press_event(event);
+    }
+
+    showOrnament() {
+        this._ornament.show();
+    }
+
+    hideOrnament() {
+        this._ornament.hide();
+    }
+});
 
 
 const Indicator = GObject.registerClass(
@@ -129,9 +218,34 @@ const Indicator = GObject.registerClass(
     _init(extension) {
         super._init(0.0, _('Project Indicator'));
 
+        /**
+         * The PopupMenu reference of the indicator
+         * @type {PopupMenu.PopupMenu} */
+        this.menu;
+
+        /** 
+         * reference to the extension object
+         * @type {ProjectChangerExtension} */
         this.extension = extension;
+
+        /**
+         * @typedef {{ children: Project[], name: string}} Project
+         */
+        /**
+         * reference to the current config object
+         * @type {{active: string, all_prjs: Project}} */
         this.config = {};
+        
+        /** 
+         * name of the active project
+         * @type {string} */
         this.active = "";
+
+        /**
+         * tracks the state of the menu
+         * @type {boolean} */
+        this.menu_open = false;
+
         this.current_path = ["default"];
         const filepath2 = GLib.build_filenamev([GLib.get_home_dir(), 'Desktop']);
         const file2 = Gio.File.new_for_path(filepath2);
@@ -142,100 +256,107 @@ const Indicator = GObject.registerClass(
             this.active = a[a.length -2];//TODO this seems wrong it should read the json file
         }
         
+        /**
+         * Text label inside the button indicator
+         * @type {St.Label}
+         */
         this.panelIcon = new St.Label({
             text: this.active,
             y_align: Clutter.ActorAlign.CENTER,
         })
-
         this.add_child(this.panelIcon);
 
-        this.menu.connect('open-state-changed', (menu, open) => {
-            this.menu_open = open;
 
+        this.menu.connect('open-state-changed', (
+            _menu,
+            /** updated open state @type {boolean} */ open
+        ) => {
+            // PanelMenu.Button automatically opens and closes its PopupMenu
+            
+            this.menu_open = open;
             if (open) {
+                // refresh the popupmenu
                 this.updateUI();
             }
         });
+
+        /**
+         * section for all projects. this will be rerendered dynamically
+         * @type {PopupMenu.PopupMenuSection} */
+        this.item_projects_section = new PopupMenu.PopupMenuSection();
+        this.menu.addMenuItem(this.item_projects_section);
+
+        // seperator between the projects and the settings
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+
+        /**
+         * reference to the Settings item in the menu
+         * @type {PopupMenu.PopupMenuItem} */
+        this.item_settings = new PopupMenu.PopupMenuItem(_('Settings'));
+        // open preferences page when settings is activated
+        this.item_settings.connect('activate', () => {
+            this.extension.openPreferences();
+        });
+        this.menu.addMenuItem(this.item_settings);
+
+        // initial update
         this.updateUI();
     }
 
     updateUI() {
+        // loads current config
         this.config = getConfig();
         this.active = this.config.active;
         
+        // sets the active project name to the indicator button
         this.panelIcon.text = this.active;
-
-        if (this.menu_open) {
-            //Menu is already open
-            return;
-        }
         
-        this.menu.removeAll();        
-        let items = []
+        // clear the section
+        this.item_projects_section.removeAll();
 
-        const list = new PopupMenu.PopupMenuSection();
-           
-        const addItem = (prj, menu, path, depth) => {
-            // section of children
+        /**
+         * Inserts all children of a project into the menu
+         * 
+         * @param {Project} project project object from the config
+         * @param {PopupMenu.PopupMenuSection | PopupMenu.PopupSubMenuMenuItem} menu parent object to insert the children into
+         * 
+         * @returns {boolean}
+         */
+        const buildProjectTree = (
+            project,
+            menu,
+        ) => {
+            let has_active = project.name == this.active;
+            if (project.children.length == 0) {
+                // the project is a leaf and can be added as a button
+                let item = new PopupMenu.PopupMenuItem(project.name);
+                menu.addMenuItem(item);
 
-            const constructChildSection = () => {
-                const section = new PopupMenu.PopupMenuSection();
-                if (prj.children.length > 0) {
-                    for (const child of prj.children) {
-                        addItem(child, section, path.concat(child.name), depth+1);
-                    }
+                if (has_active) item.setOrnament(PopupMenu.Ornament.CHECK);
+
+                item.connect('activate', () => {
+                    this.change_project(project.name);
+                });
+            } else {
+                // the project has children and needs to be added as a submenu
+                const submenu = new Foldout(project.name);
+                
+                if(has_active) submenu.showOrnament();
+                submenu._label.connect('clicked', () => {
+                    this.change_project(project.name);
+                });
+
+                menu.addMenuItem(submenu);
+                let were_active = has_active;
+                for (const child of project.children) {
+                    has_active |= buildProjectTree(child, submenu);
                 }
-                return section;
+                if (has_active && !were_active) submenu.open();
             }
+            return has_active;
+        }
 
-            let in_sub_menu = false;
-
-            const onItemActivated = () => {
-                if (prj.children.length == 0 || in_sub_menu) {
-                    this.change_project(prj.name);
-                    menu._getTopMenu().close();
-                    // this.updateUI();
-                } else {
-                    if (depth > 0) {
-                        this.change_project(prj.name);
-                    }
-                    for (const it of items) {
-                        if (it != item) {
-                            it.destroy();
-                        }
-                    }
-                    item.setIcon(Gio.icon_new_for_string('pan-down-symbolic'))
-                    in_sub_menu = true;
-                    list.addMenuItem(constructChildSection());
-                }
-            }
-
-            let item = menu.addAction(prj.name, () => {
-                onItemActivated();
-            }, (prj.children.length > 0 && depth != 0) ? 'pan-end-symbolic' : '');
-            // item._icon.setIconSize(16);
-            if (prj.children.length > 0) {
-                item._getTopMenu().itemActivated = () => {};
-            }
-            if (this.active != "" && prj.name == this.active) {
-                item.setOrnament(PopupMenu.Ornament.CHECK)
-            }
-            items.push(item);
-
-            if (depth == 0) {
-                onItemActivated();
-            }
-        };
-
-        addItem(this.config.all_prjs, list, ["default"], 0);
-
-        this.menu.addMenuItem(list);
-        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-        const new_project = new PopupMenu.PopupMenuItem(_('Settings'));
-        new_project.connect('activate', () => {
-            this.extension.openPreferences();
-        });
-        this.menu.addMenuItem(new_project);
+        buildProjectTree(this.config.all_prjs, this.item_projects_section);
     }
 
     change_project(name) {
@@ -245,6 +366,7 @@ const Indicator = GObject.registerClass(
             [GLib.build_filenamev([GLib.get_home_dir(), '.local/bin/change-prj']), name],//TODO find a better way to get the path of the executable 
             Gio.SubprocessFlags.NONE
         );
+        this.menu.close();
     }
 });
 
@@ -332,10 +454,6 @@ export default class ProjectChangerExtension extends Extension {
         return new Gio.Settings({ settings_schema: schemaObj });
     }
 }
-
-// function init(meta) {
-//     return new   (meta.uuid);
-// }
 
 class DbusService {
     Reload() {
