1204e7dd9b0d80f927362e5275ef4ebd141bef14
Moritz Salger <slg@biba.uni-bremen.de>
2024-04-10 10:35:22 +0200

    Added GPL-3 headers

--- extension.js
+++ extension.js
@@ -1,25 +1,24 @@
-/* extension.js
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
+/**
+Wechsel
+Copyright (C) 2024 JustSomeRandomUsername
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+SPDX-License_identifier: GPL-3.0-or-later
+*/
 
 /* exported init */
-
-// const GETTEXT_DOMAIN = 'project-switcher-extension';
-
 import St from 'gi://St';
 import Atk from 'gi://Atk';
 import GObject from 'gi://GObject';
